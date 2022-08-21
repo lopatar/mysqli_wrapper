3af56db08f0c57cc661efcd9d3856d0ac8a6dc80
Jiří Lopatář <github@lopatar.me>
2022-08-21 10:58:52 +0200

    Update to PHP8

--- src/mysqli_wrapper.php
+++ src/mysqli_wrapper.php
@@ -1,43 +1,51 @@
 <?php
+declare(strict_types=1);
 
-class mysqli_wrapper {
+namespace app\database;
 
-    private $connection;
+use Exception;
+use mysqli;
+use mysqli_result;
 
-    public function __construct($host, $username, $password, $db_name) {
-        $this->connection = new mysqli($host, $username, $password, $db_name); //create mysqli object
+final class mysqli_wrapper
+{
+	private mysqli $connection;
 
-        if (!$this->connection)
-            throw new Exception('Error connecting to DB'); 
-    }
+	public function __construct(string $host, string $username, string $password, string $db_name) {
+		$this->connection = new mysqli($host, $username, $password, $db_name); //create mysqli object
+	}
 
-    public function __destruct() {
-        $this->connection->close(); //close connection on destructor
-    }
+	public function __destruct() {
+		$this->connection->close(); //close connection on destructor
+	}
 
-    public function query(string $query, array $args = [], string $types = null) {
-        if ($types === null && $args !== []) //if we have arguments and no types are specified we act as if they were all strings
-            $types = str_repeat('s', count($args));
+	/**
+	 * @throws Exception
+	 */
+	public function query(string $query, array $args = [], string $types = null): mysqli_result|false {
+		if ($types === null && $args !== []) //if we have arguments and no types are specified we act as if they were all strings
+			$types = str_repeat('s', count($args));
 
-        $stmt = $this->connection->prepare($query); //prepare the query
+		$stmt = $this->connection->prepare($query); //prepare the query
 
-        if (!$stmt)
-            throw new Exception('Error preparing query');
+		if (!$stmt) {
+			throw new Exception('Error preparing query');
+		}
 
-        if (strpos($query, '?') !== false)
-            $stmt->bind_param($types, ...$args); //bind our parameters to the query
+		if (str_contains($query, '?')) {
+			$stmt->bind_param($types, ...$args); //bind our parameters to the query
+		}
 
-        $stmt->execute(); //execute our query
+		$stmt->execute(); //execute our query
 
-        $result = $stmt->get_result(); //get query result
+		$result = $stmt->get_result(); //get query result
 
-        $stmt->close(); //close db handle
+		$stmt->close(); //close db handle
 
-        return $result; //return result
-    }
+		return $result; //return result
+	}
 
-    public function get_connection(): mysqli {
-        return $this->connection;
-    }
-
+	public function get_connection(): mysqli {
+		return $this->connection;
+	}
 }
